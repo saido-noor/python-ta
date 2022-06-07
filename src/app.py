920d7d6e3871ba3546d95b5f8d655c8e2496729e
saido-noor <saidoomarnoor@gmail.com>
2022-06-07 18:16:30 +0300

    finished the test

--- src/app.py
+++ src/app.py
@@ -4,9 +4,20 @@ import os
 
 def prime(s):
     # your code goes here
+    if s> 1:  
+        for n in range(2,s):  
+            if (s % n) == 0:  
+                return False
+        return True
+    else:
+        return False
+print(prime(64))
+print(prime(5))
+
 
 def solution(s):
     return prime(s)
+solution(20)
 
 
 if __name__ == "__main__":
